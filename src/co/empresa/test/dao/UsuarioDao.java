21a71b1e18acb0a686b710f5c4acf10449ab806e
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2022-10-24 20:53:09 -0500

    Creacion Servlet

--- src/co/empresa/test/dao/UsuarioDao.java
+++ src/co/empresa/test/dao/UsuarioDao.java
@@ -1,5 +1,107 @@
 package co.empresa.test.dao;
 
-public class UsuarioDao {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import co.empresa.test.modelo.Usuario;
+import co.empresa.test.util.Conexion;
+
+public class UsuarioDao {
+	
+	private Conexion conexion;
+
+	public static final String INSERT_USUARIO_SQL = "INSERT INTO usuario (nombre,email,pais) VALUES (?,?,?);";
+	public static final String DELETE_USUARIO_SQL = "DELETE FROM usuario WHERE id = ?;";
+	public static final String UPDATE_USUARIO_SQL = "USUARIO usuario SET nombre=?,email=?,pais=? WHERE id=?;";
+	public static final String SELECT_USUARIO_BY_ID = "SELECT * FROM usuario WHERE id = ?;";
+	public static final String SELECT_ALL_USUARIO = "SELECT * FROM usuario;";
+	
+	public UsuarioDao() {
+		this.conexion = Conexion.getConexion();
+	}
+	
+	public void insert(Usuario usuario) {
+		try {
+			PreparedStatement preparedStatement = conexion.setPreparedStatement(INSERT_USUARIO_SQL);
+			preparedStatement.setString(1, usuario.getNombre());
+			preparedStatement.setString(2, usuario.getEmail());
+			preparedStatement.setString(3, usuario.getPais());
+			conexion.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void delete(int id) {
+		try {
+			PreparedStatement preparedStatement = conexion.setPreparedStatement(DELETE_USUARIO_SQL);
+			preparedStatement.setInt(1, id);
+			conexion.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update(Usuario usuario) {
+		try {
+			PreparedStatement preparedStatement = conexion.setPreparedStatement(INSERT_USUARIO_SQL);
+			preparedStatement.setString(1, usuario.getNombre());
+			preparedStatement.setString(2, usuario.getEmail());
+			preparedStatement.setString(3, usuario.getPais());
+			preparedStatement.setInt(4, usuario.getId());
+			conexion.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Usuario> selectAll(){
+		List<Usuario> usuarios = new ArrayList<>();
+		
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(SELECT_ALL_USUARIO);
+			
+			ResultSet rs = conexion.query();
+			
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String nombre = rs.getString("nombre");
+				String email = rs.getString("email");
+				String pais = rs.getString("pais");
+				usuarios.add(new Usuario(id,nombre,email,pais));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return usuarios;
+	}
+	
+	public List<Usuario> select(int id){
+		Usuario usuario = null;
+		
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(SELECT_USUARIO_BY_ID);
+			preparedStatement.setInt(1, id);
+			
+			ResultSet rs = conexion.query();
+						
+			while(rs.next()) {
+				String nombre = rs.getString("nombre");
+				String email = rs.getString("email");
+				String pais = rs.getString("pais");
+				usuario = new Usuario(id,nombre,email,pais));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return usuario;
+	}
 }
+
